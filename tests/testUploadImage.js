5cc01614b3fcaa209f08988a8bf7394cec792f44
Alex <a.belko93@gmail.com>
2018-07-06 18:30:04 +0300

    save testUploadImage

--- tests/testUploadImage.js
+++ tests/testUploadImage.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 const { url } = require('./../creds');
 async function testUploadImage(page) {
   const UPLOAD_BUTTON_SELECTOR = 'body > div.lt-f-col.main-app-wrapper.au-target > div > ct-form > form > div > compose:nth-child(4) > ct-form-group:nth-child(1) > fieldset > div > div.lt-f-3.lt-f-col.form-group-error > ct-image-upload > div > div > div.ct-image-upload__message > div > label > span'
-  const errorFunction = (errorMessage) => { throw Error(errorMessage) }
+  const errorFunction = (errorMessage) => { throw Error(errorMessage) };
   page.on('pageerror', errorFunction);
   await page.goto(url + '/#/app/display-creative/create');
   const files = fs.readdirSync(path.join(process.cwd(), './assets/docs/'));
   console.log('files', files[0])
-  // await page.waitForSelector(UPLOAD_BUTTON_SELECTOR);
-  
-  await page.waitForSelector('body > div.lt-f-col.main-app-wrapper.au-target > div > ct-form > form > div > compose:nth-child(4) > ct-form-group:nth-child(2) > fieldset > div > div.lt-f-3.lt-f-col.form-group-error > dsp-add-new > div > a');
+  await page.waitForSelector(UPLOAD_BUTTON_SELECTOR);
+
   const input = await page.$('input[type=file]');
   await input.uploadFile(files[0]);
 
